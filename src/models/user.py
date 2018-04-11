f88c5075b61bf85712ae583b275577382a258823
Calvin Chan <calvinch4n@gmail.com>
2018-04-11 01:21:05 -0400

    pretty colors

--- src/models/user.py
+++ src/models/user.py
@@ -1,33 +1,33 @@
-from db import db
+# from db import db
 
-class UserModel(db.Model):
-    __tablename__ = 'LINKEDINUSER'
-    id = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+# class UserModel(db.Model):
+#     __tablename__ = 'LINKEDINUSER'
+#     id = db.Column(db.String, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
 
-    works = db.relationship('WORK', lazy='dynamic')
-    educations = db.relationship('EDUCATION', lazy='dynamic')
+#     works = db.relationship('WORK', lazy='dynamic')
+#     educations = db.relationship('EDUCATION', lazy='dynamic')
     
-    def __init__(self, name):
-        self.name = name
+#     def __init__(self, name):
+#         self.name = name
 
-    def json(self):
-        return {'name':self.name,
-                'work': [work.json() for work in self.works.all()],
-                'education':[education.json() for education in self.educations.all()]}
+#     def json(self):
+#         return {'name':self.name,
+#                 'work': [work.json() for work in self.works.all()],
+#                 'education':[education.json() for education in self.educations.all()]}
 
-    @classmethod
-    def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()
+#     @classmethod
+#     def find_by_name(cls, name):
+#         return cls.query.filter_by(name=name).first()
 
-    @classmethod
-    def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id).first()
+#     @classmethod
+#     def find_by_id(cls, _id):
+#         return cls.query.filter_by(id=_id).first()
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+#     def save_to_db(self):
+#         db.session.add(self)
+#         db.session.commit()
 
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
+#     def delete_from_db(self):
+#         db.session.delete(self)
+#         db.session.commit()
--- src/resources/user.py
+++ src/resources/user.py
@@ -1,32 +1,32 @@
-from flask_restful import Resource, reqparse
-from models.users import UserModel
+# from flask_restful import Resource, reqparse
+# from models.users import UserModel
 
-#Use this to find user by id can also delete the user
-#Example:
-#GET
-#{url}/user/<int: id>
-#RETURNS {name:str, work:listof(WORK), education:listof(EDUCATION)}
+# #Use this to find user by id can also delete the user
+# #Example:
+# #GET
+# #{url}/user/<int: id>
+# #RETURNS {name:str, work:listof(WORK), education:listof(EDUCATION)}
 
-#DELETE
-#{url}/user/<int: id>
-#RETURNS {message:str}
+# #DELETE
+# #{url}/user/<int: id>
+# #RETURNS {message:str}
 
-class User(Resource):
-    parser = reqparse.RequestParser()
+# class User(Resource):
+#     parser = reqparse.RequestParser()
 
-    def get(self, id):
-        user = UserModel.find_by_id(int(id))
+#     def get(self, id):
+#         user = UserModel.find_by_id(int(id))
 
-        if user:
-            return user.json()
-        else:
-            return {'message': 'User not in Database'}, 404
+#         if user:
+#             return user.json()
+#         else:
+#             return {'message': 'User not in Database'}, 404
 
-    @jwt_required()
-    def delete(self, id):
-        user = UserModel.find_by_id(int(id))
+#     @jwt_required()
+#     def delete(self, id):
+#         user = UserModel.find_by_id(int(id))
 
-        if user:
-            user.delete_from_db()
+#         if user:
+#             user.delete_from_db()
 
-        return {'message': 'User Deleted :D'}
+#         return {'message': 'User Deleted :D'}
